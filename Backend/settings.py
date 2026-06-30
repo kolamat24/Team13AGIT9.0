984b80e46ca294d7a13a22268f50815158937581
Augusta Edem <austafrancisedem@gmail.com>
2026-06-30 04:20:12 +0100

    fix: make email sending async to prevent server crash

--- Backend/settings.py
+++ Backend/settings.py
@@ -160,8 +160,7 @@ SIMPLE_JWT = {
 AUTH_USER_MODEL = "aerogleam_app.User"
 
 # Email Configuration
-#EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
